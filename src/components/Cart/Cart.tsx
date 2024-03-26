011d1d9ba6b467374d59dde67778278dc173fe04
Maryy2024 <mariae_20@virgilio.it>
2024-03-26 15:02:00 +0100

    feat: Cart

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,25 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import Product from '../../types/Product';
+import Card from './../../components/Card/Card';
 
-type Products = {
-    quantity: number
+type CartProps = {
+    price: number,
+    products: []
 }
 
-const Cart = ({}) => {
+const Cart = ({price, products}: CartProps) => {
 
-    const [totQuantity, setTotQuantity] = useState<number>(0);
-    const [totPrice, setTotPrice] = useState<number>(0)
+    const [totQuantity, setTotQuantity] = useState<number>(0)
+    const [totPrice, setTotPrice] = useState<number>(0);
 
     useEffect(() => {
-      const quantityProducts = 
-
-    }, [])
-
+        setTotQuantity(products.reduce((acc: number, products: any) => acc + products.quantity, 0));
+        setTotPrice(products.reduce((acc: number, products: Product)=> acc + products.price * products.quantity, 0));
+      }, [products])
+    
+    
 
   return (
-    <div>
-      
-    </div>
+
+         <div className="container_cart_quantity">
+        <div className={totQuantity === 0 ? "hidden" : ""}>{`${price.toFixed(2)}€`}</div>
+        <div className="cart-container">
+            <img className="cart" src="cart.png" alt="Cart"></img>
+        </div>
+        <div className={`cart-container_tot ${totQuantity === 0 ? "hidden" : ""}`}>{totQuantity === 0 ? "" : totQuantity}</div>
+        </div>
   );
 }
 
