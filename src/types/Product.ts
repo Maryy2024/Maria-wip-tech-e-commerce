56946873bc1371797c4796a90ba480896a6b2a2c
Matteo Francesco Puglisi <matteopuglisif@gmail.com>
2024-03-27 21:31:33 +0100

    fix: Adjusted Product Interface

--- src/types/Product.ts
+++ src/types/Product.ts
@@ -1,13 +1,13 @@
 interface Product {
-    id: number
+    id?: number
     title: string
     price: number
     description: string
     category: string
     image: string
-    rating: {
-        rate: number
-        count: number
+    rating?: {
+        rate?: number
+        count?: number
     }
 }
 export default Product;
